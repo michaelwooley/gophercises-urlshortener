85fa6e50dc8e9fd1f531552f215eb852506f04db
Michael Wooley <wm.wooley@gmail.com>
2021-01-01 12:18:19 -0500

    YAML handler done

--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/michaelwooley/gophercises-urlshortener
 go 1.15
 
 replace github.com/michaelwooley/gophercises-urlshortener/urlshort v0.0.0 => ./urlshort
+
+require (
+	github.com/kr/text v0.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0
+)
--- urlshort/handler.go
+++ urlshort/handler.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -12,18 +14,31 @@ import (
 // that each key in the map points to, in string format).
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handler {
+func MapHandler(pathsToUrls map[string]string, kind string, fallback http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		prefix := r.URL.Path
 		val, ok := pathsToUrls[r.URL.Path]
 		if !ok {
-			log.Println(fmt.Sprintf("Prefix `%s` NOT found. Redirecting to default.", prefix))
+			log.Println(fmt.Sprintf("Prefix `%s` NOT found in %s. Redirecting to fallback.", prefix, kind))
 			fallback.ServeHTTP(w, r)
 		} else {
-			log.Println(fmt.Sprintf("Prefix `%s` found. Redirecting to: %s", prefix, val))
+			log.Println(fmt.Sprintf("Prefix `%s` found in %s. Redirecting to: %s", prefix, kind, val))
 			http.Redirect(w, r, val, http.StatusSeeOther)
 		}
 	})
+}
+
+type pathsToUrlsYamlInterface []struct {
+	// NOTE: Will not print out unless the elements are public!
+	Path string
+	URL  string
+}
+
+func (c *pathsToUrlsYamlInterface) Parse(data []byte) error {
+	if err := yaml.Unmarshal(data, c); err != nil {
+		return err
+	}
+	return nil
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -42,7 +57,18 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+func YAMLHandler(yml []byte, fallback http.Handler) (http.Handler, error) {
+	var pathsToUrlsYaml pathsToUrlsYamlInterface
+
+	if err := pathsToUrlsYaml.Parse(yml); err != nil {
+		return nil, err
+	}
+	log.Println(fmt.Sprintf("Found %d url shortcuts", len(pathsToUrlsYaml)))
+
+	pathsToUrls := make(map[string]string)
+	for _, el := range pathsToUrlsYaml {
+		pathsToUrls[el.Path] = el.URL
+	}
+
+	return MapHandler(pathsToUrls, "YAML", fallback), nil
 }
